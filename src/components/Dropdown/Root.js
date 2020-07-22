eac4d157f1d8780d5c62fc6f093b9230e36a5ed5
pedroksty <pedrotrab2132@gmail.com>
2020-07-22 19:54:08 -0300

    Add trasition do second motion in dropdown-root

--- src/components/Dropdown/Root.js
+++ src/components/Dropdown/Root.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import { motion } from 'framer-motion'
 
 import { Context } from './Provider'
@@ -6,16 +6,69 @@ import { DropdownSection } from './Section'
 
 // import { Container } from './styles';
 
+const refDuration = .22
+
 export function DropdownRoot() {
-  const { options } = useContext(Context)
+  const { options, cachedId, getOptionById, targetId } = useContext(Context)
+
+  const cachedOption = useMemo(() => getOptionById(cachedId), [cachedId, getOptionById])
+
+  let [width, height, x] = [0, 0, 0]
+
+  if (cachedOption) {
+    const { optionCenterX, contentDimensions } = cachedOption
+
+    width = contentDimensions?.width
+    height = contentDimensions?.height
+
+    x = optionCenterX - width / 2
+  }
+
+  const [hovering, setHovering] = useState(false);
+
+  const isActive = targetId !== null || hovering
+
+  const [hasInteracted, setHasInteracted] = useState(false)
+
+  const isFirstInteraction = isActive && !hasInteracted
+
+  if (isFirstInteraction) {
+    setTimeout(() => {
+      if (!hasInteracted) setHasInteracted(true)
+    }, 15)
+  }
+
+
 
   return (
     <div className="dropdown-root">
-      <div>
-        <div className="dropdown-container">
+      <motion.div className="dropdown-container"
+        animate={{
+          x,
+          width,
+          height,
+          pointerEvents: isActive ? 'unset' : 'none'
+        }}
+        transition={{
+          ease: 'easeOut',
+          x: isFirstInteraction ? { duration: 0 } : refDuration,
+          width: { duration: isFirstInteraction ? 0 : refDuration * 0.93 },
+          height: { duration: isFirstInteraction ? 0 : refDuration * 0.93 },
+          pointerEvents: { delay: 0.05 }
+        }}
+
+        onHoverStart={() => setHovering(true)}
+        onHoverEnd={() => setHovering(false)}
+      >
+        <motion.div
+          animate={{
+            x: -x
+          }}
+
+        >
           {options.map(item => <DropdownSection key={item.id} option={item} />)}
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
